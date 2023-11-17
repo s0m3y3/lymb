928f056943d9a740d00f92c9a04fed3c6039ec8e
leex3683 <132837134+leex3683@users.noreply.github.com>
2023-11-16 20:58:28 -0600

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/lymb');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://yaX102:qGyiTIORHrOTuADD@yax102.tkf597m.mongodb.net/');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/lymb');
+
 module.exports = mongoose.connection;
