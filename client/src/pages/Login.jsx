a9c469ad16f3984c443e604075e2a387b4410458
IsabellaKingson <isabellasophiaking@gmail.com>
2023-11-16 22:23:03 -0600

    Created a theme file to hold our custom fonts and colors

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,28 +1,22 @@
 import LoginForm from "../components/LoginForm";
-import SignupForm from "../components/SignupForm";
+// import SignupForm from "../components/SignupForm";
 import { Container, Button } from "@chakra-ui/react";
 import { useState } from "react";
 
 // TODO: Create a signup button that conditionally hides the login form and shows the signup form
-const [signUp, setSignUp] = useState(false);
-const signupHandler = () => {
-  setSignUp(true);
-};
+
+// const signupHandler = () => {
+//   const [signUp, setSignUp] = useState(false);
+//   setSignUp(true);
+// };
+
 const Login = () => {
-  if (signUp === false) {
-    return (
-      <Container>
-        <LoginForm />
-        <Button onClick={signupHandler}>Sign up</Button>
-      </Container>
-    );
-  } else if (signUp === true) {
-    return (
-      <Container>
-        <SignupForm />
-      </Container>
-    );
-  }
+  return (
+    <Container>
+      <LoginForm />
+      {/* <SignupForm /> */}
+    </Container>
+  );
 };
 
 export default Login;
