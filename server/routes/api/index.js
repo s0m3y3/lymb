a82637c066588e43bd0daefc64a667797a390537
Yee Y <s0m3y3@gmail.com>
2023-11-18 14:31:16 -0600

    Updated query for exercise in resolver & typeDefs. debugged exercise.json seed file.

--- server/routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const exerciseRoutes = require('./exerciseRoutes');
-// const workoutRoutes = require('./workoutRoutes');
-
-router.use('/exercise', exerciseRoutes);
-// router.use('/workout', workoutRoutes);
-
-module.exports = router;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -12,12 +12,30 @@ const typeDefs = `
 
   type Query {
     me: User
+    exercise: [Exercise] 
   }
 
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
+    createExercise(input: ExerciseInput!): Exercise
   }
+
+  input ExerciseInput {
+    type: String!
+    exerciseName: String!
+    description: String!
+    target: String
+  }
+
+  type Exercise {
+    _id: ID!
+    type: String!
+    name: String!
+    description: String!
+    target: String
+  }
+
 `;
 
 module.exports = typeDefs;
