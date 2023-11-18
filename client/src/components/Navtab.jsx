c76ec11a4b0e619ad0e085552b86737e8476ccb7
unknown <leex3683@umn.edu>
2023-11-18 17:29:00 -0600

    hide nav links unless logged in. Added logout button

--- client/src/components/Navtab.jsx
+++ client/src/components/Navtab.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import { Tabs, TabList, TabPanels, Tab, TabPanel, Button } from '@chakra-ui/react'
+import Auth from '../utils/auth';
+
 const Navbar = () => {
   return (
 <Tabs>
   <TabList>
     <Tab as={Link} to="/">Home</Tab>
+
+    {Auth.loggedIn() ? (
+      <>
+      <Tab as={Link} to="/dashboard">Dashboard</Tab>
+      <Tab as={Link} to="/browse">Browse</Tab>
+      <Button colorScheme='white' textColor={'black'} size='md' onClick={Auth.logout}>Logout</Button>
+      </>
+    ) : (    
     <Tab as={Link} to="/login">Login</Tab>
-    <Tab as={Link} to="/dashboard">Dashboard</Tab>
-    <Tab as={Link} to="/browse">Browse</Tab>
+    )}
+    
   </TabList>
 
 </Tabs>
