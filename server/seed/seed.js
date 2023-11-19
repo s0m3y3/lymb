dad2be5de1cfbe9fe8079bec2eed377648fe9af1
Yee Y <s0m3y3@gmail.com>
2023-11-19 11:35:36 -0600

    updated seed data with password hash. Added random workout to users.

--- server/seed/seed.js
+++ server/seed/seed.js
@@ -1,5 +1,6 @@
 const connection = require('../config/connection');
 const { Exercise, User, Workout } = require('../models');
+const bcrypt = require('bcrypt'); //for password hashing
 
 //importing json data for seeding. 
 const exerciseData = require('./exercise.json')
@@ -14,29 +15,66 @@ connection.on('error', (err) => {
 
 connection.once('open', async () => {
   try {
-    let userCheck = await connection.db.listCollections({ name: 'users' }).toArray();
-    if (userCheck.length) {
-      await connection.dropCollection('users');
-      console.log('Users db dropped.');
-    }
-    await User.collection.insertMany(userData);
-    console.log('Users seeded.');
-
+    //drops exercise data
     let exerciseCheck = await connection.db.listCollections({ name: 'exercise' }).toArray();
+    // console.log(exerciseCheck)
     if (exerciseCheck.length) {
       await connection.dropCollection('exercise');
       console.log('Exercise db dropped.');
     }
+    //seed exercise data
     await Exercise.collection.insertMany(exerciseData);
     console.log('Exercises seeded.');
 
+    //drops workout data
     let workoutCheck = await connection.db.listCollections({ name: 'workout' }).toArray();
     if (workoutCheck.length) {
       await connection.dropCollection('workout');
       console.log('Workout db dropped.');
     }
+    //seed workout
     await Workout.collection.insertMany(workoutData);
     console.log('Workouts seeded.');
+
+    //drops users data
+    let userCheck = await connection.db.listCollections({ name: 'users' }).toArray();
+    if (userCheck.length) {
+    await connection.dropCollection('users');
+    console.log('Users db dropped.');
+    }
+
+    //function to grab workout from database - needed for _ID. 
+    const databaseWorkout = await Workout.find();
+
+    //function that randomly returns, 0-3 workout
+    async function getRandomWorkoutIDs() {
+      //random number generator, 0-3
+      const numWorkouts = Math.floor(Math.random() * 4);
+      //grabs workout.
+      const setWorkout = databaseWorkout.slice(0, numWorkouts);
+      //pulls only workout ID. 
+      const setWorkoutID = setWorkout.map((workout) => workout._id);
+      return setWorkoutID;
+    }
+
+    //to hash the password in userdata
+    const hashedUsers = await Promise.all(
+      userData.map(async (user) => {
+        const hashedPassword = await bcrypt.hash(user.password, 10); // Hash the password
+        // Create new user with hashed password
+        setWorkout = await getRandomWorkoutIDs();
+        // console.log(abc)
+        return {
+          username: user.username,
+          email: user.email,
+          password: hashedPassword,
+          workouts: setWorkout
+        };
+      })
+    );
+
+    await User.collection.insertMany(hashedUsers);   
+    console.log('Users seeded.');
 
     console.log('FINISHED seeding Data.');
     process.exit(0);
