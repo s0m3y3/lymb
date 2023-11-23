7bbe2761056400663903a9939337232182e61894
IsabellaKingson <isabellasophiaking@gmail.com>
2023-11-22 21:36:05 -0600

    Solidified how the backend mutates workouts

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -23,7 +23,30 @@ const typeDefs = `
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
+    createWorkout(input: CreateWorkoutInput!): Workout
+    deleteWorkout(input: WorkoutInput!): Workout
+    updateWorkout(input: WorkoutInput!): Workout
+  }
 
+  input CreateWorkoutInput {
+    name: String!
+    description: String!
+    exercises: [String]
+  }
+
+  input WorkoutInput {
+    _id: ID!
+    name: String
+    description: String
+    exercises: [String]
+  }
+
+  input Exercise {
+    _id: ID!
+    type: String!
+    name: String!
+    description: String!
+    target: String!
   }
 
   type Exercise {
@@ -37,49 +60,47 @@ const typeDefs = `
   type Workout {
     _id: ID!
     name: String!
+    description: String!
     exercises: [Exercise]
   }
 `;
 
 module.exports = typeDefs;
 
-
 // input ExerciseInput {
-  //   _id: ID!
-  //   type: String!
-  //   name: String!
-  //   description: String!
-  //   target: String!
-  // }
+//   _id: ID!
+//   type: String!
+//   name: String!
+//   description: String!
+//   target: String!
+// }
 
-  // input ExerciseUpdate {
-  //   type: String
-  //   name: String
-  //   description: String
-  //   target: String
-  // }
-  // input WorkoutInput {
-  //   _id: ID!
-  //   name: String!
-  //   exercises: [ID]
-  // }
+// input ExerciseUpdate {
+//   type: String
+//   name: String
+//   description: String
+//   target: String
+// }
+// input WorkoutInput {
+//   _id: ID!
+//   name: String!
+//   exercises: [ID]
+// }
 
-  // input WorkoutUpdate {
-  //   _id: ID
-  //   name: String
-  //   exercises: [ID]
-  // }
+// input WorkoutUpdate {
+//   _id: ID
+//   name: String
+//   exercises: [ID]
+// }
 
-  //mutations
-  // saveExercise(input: ID!): User
-  // removeExercise(input: ID!): User
+//mutations
+// saveExercise(input: ID!): User
+// removeExercise(input: ID!): User
 
-  // createExercise(input: ExerciseInput!): Exercise
-  // updateExercise(input: ExerciseUpdate!): Exercise
-  // deleteExercise(input: ID!): Exercise
+// createExercise(input: ExerciseInput!): Exercise
+// updateExercise(input: ExerciseUpdate!): Exercise
+// deleteExercise(input: ID!): Exercise
 
-  // createWorkout(input: WorkoutInput!): Workout
-  // updateWorkout(input: WorkoutUpdate!): Workout
-  // deleteWorkout(input: ID!): Workout
-
-
+// createWorkout(input: WorkoutInput!): Workout
+// updateWorkout(input: WorkoutUpdate!): Workout
+// deleteWorkout(input: ID!): Workout
