b51daf400553b225da562b0b62439fcda6e5b6c2
Yee Y <s0m3y3@gmail.com>
2023-11-26 07:57:25 -0600

    Added collapse bar to Browse Page. WorkoutQuery Works.

--- client/src/pages/Browse.jsx
+++ client/src/pages/Browse.jsx
@@ -11,11 +11,20 @@ import {
   Container,
   VStack,
   Wrap,
+  IconButton,
+  Collapse,
 } from "@chakra-ui/react";
+import {
+  UPDATE_WORKOUT,
+  DELETE_WORKOUT,
+  CREATE_WORKOUT,
+} from "../utils/mutations.js";
+import { HamburgerIcon, ChevronUpIcon } from '@chakra-ui/icons';
+import Auth from "../utils/auth";
 import AddToWorkoutModal from "../components/AddToWorkoutModal.jsx";
 import theme from "../components/theme";
 import "@fontsource-variable/lexend-peta"; //font theme. 
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_EXERCISE, QUERY_WORKOUT} from '../utils/queries.js'; 
 import React, {useEffect, useState} from 'react';
 import getBrowseData from '../utils/browserImport.js' //Generates images and name for "Browse By Type"
@@ -27,10 +36,9 @@ const Browse = () => {
   const exerciseDataJson = data?.exercises || [];
 
   const {loading: workoutLoading, data: workoutData, error: workoutError } = useQuery(QUERY_WORKOUT);
-  const WorkoutDataJson = workoutData?.exercises || [];
-  console.log(WorkoutDataJson)
+  const WorkoutDataJson = workoutData?.workouts || [];
+  // console.log(WorkoutDataJson)
 
-// console.log(data)
   const [selectedType, setSelectedType] = useState(''); //Browse-Type that the user selects.
   const [exerciseData, setExerciseData] = useState(exerciseDataJson); //modified exercisedata to be displayed in results
   const [exerciseId, setExerciseId] = useState('');
@@ -69,6 +77,27 @@ useEffect(() => {if (data) {setExerciseData(data.exercises || []);}}, [data]);
     }
   };
 
+  // This is for Collapsible button of "Browse by Workout".
+  const [showWorkouts, setShowWorkouts] = useState(true); // State to control visibility of workouts
+  const handleToggleWorkouts = () => {
+    setShowWorkouts(!showWorkouts); // Toggle show/hide on hamburger icon click
+  };
+  const addWorkout = (addId)=>{
+    console.log(addId)  
+  }
+
+  // This is for Collapsible button of "Browse by Type".
+  const [showType, setShowType] = useState(true); // State to control visibility of workouts
+  const handleToggleType = () => {
+    setShowType(!showType); // Toggle show/hide on hamburger icon click
+  };
+
+  // This is for Collapsible button of "Results".
+  const [showResults, setShowResults] = useState(true); // State to control visibility of workouts
+  const handleToggleResults = () => {
+    setShowResults(!showResults); // Toggle show/hide on hamburger icon click
+  };
+    
   return (
     <Container maxW="100%">
 
@@ -76,25 +105,49 @@ useEffect(() => {if (data) {setExerciseData(data.exercises || []);}}, [data]);
     <Box my={10} display="flex" flexDirection={'column'}>
 
 {/* Browse by Workout */}
-{/* <Heading mb={5} alignSelf='center' fontFamily={theme.fonts.heading} as="h2" size="md">
-        Browse by Workout
-      </Heading>
-      <Flex overflowX="auto" >
+    <Heading mb={5} alignSelf='center' fontFamily={theme.fonts.heading} as="h2" size="md">
+      Browse by Workout
+        <IconButton
+          ml={5}
+          icon={showWorkouts ? <ChevronUpIcon /> : <HamburgerIcon />}
+          aria-label="Toggle workouts"
+          onClick={handleToggleWorkouts}
+        />
+    </Heading>
+    {/* showWorkout below collapse this entire field */}
+    {showWorkouts && ( <> 
+      <Flex overflowX="auto" mb={5}>
       {WorkoutDataJson.map((workout, index) => (
-        <Card key={index}  m={2} w="150px" h="150px" minW="150px" minH="150px"  maxW="150px" maxH="150px">
-          <CardHeader fontWeight="bold" fontSize="large">{workout.name}</CardHeader>
+        <Card key={index} m={2} w="150px" h="150px" minW="200px" minH="150px" maxW="200px" maxH="150px">
+          <CardHeader fontWeight="bold" fontSize="large">
+            {workout.name.length > 40 ? `${workout.name.substring(0, 40)}...` : workout.name}
+          </CardHeader>
           <VStack flex={1} justify="flex-end" alignItems="flex-start">
-            <Button minH="40px" minW="150px">Click to add</Button>
+                    <Button
+                      minH="40px"
+                      minW="200px"
+                      onClick={()=>addWorkout(workout.id)}
+                    >Click to add
+                    </Button>
           </VStack>
         </Card>
         ))}
-    </Flex> */}
+      </Flex>
+      </>
+    )}
 
       <Heading mb={5} alignSelf='center' fontFamily={theme.fonts.heading} as="h2" size="md">
         Browse by Type
+          <IconButton
+            ml={5}
+            icon={showType ? <ChevronUpIcon /> : <HamburgerIcon />}
+            aria-label="Toggle Type"
+            onClick={handleToggleType}
+          />
       </Heading>
       <Wrap justify={"space-evenly"}>
         {/* Maps through browseType, to populate the cards TYPE. */}
+        {showType && ( <> 
         {typeData.map((data, index) => (
           <Card
             key={`type-${index}`}
@@ -117,7 +170,9 @@ useEffect(() => {if (data) {setExerciseData(data.exercises || []);}}, [data]);
               {data.type}
             </CardBody>
           </Card>
-        ))}
+          ))}
+         </>
+        )}
       </Wrap>
     </Box>
 
@@ -125,25 +180,34 @@ useEffect(() => {if (data) {setExerciseData(data.exercises || []);}}, [data]);
       <Box my={10} display='flex' flexDirection={'column'}>
         <Heading mb={5} alignSelf='center' fontFamily={theme.fonts.heading} as="h2" size="md">
           Results
+          <IconButton
+            ml={5}
+            icon={showResults ? <ChevronUpIcon /> : <HamburgerIcon />}
+            aria-label="Toggle Results"
+            onClick={handleToggleResults}
+          />
         </Heading>
         <SimpleGrid spacing={10} minChildWidth={300} textAlign="center">
   {/* Maps through exerciseData (includes filtering), to populate the cards. */}
-            {exerciseData.map((exercise, index) => (
-            <Card key={exercise._id}>
-              <CardHeader fontWeight="bold" fontSize="large">{exercise.name}</CardHeader>
-              <CardBody>{exercise.description}</CardBody>
-              <CardBody>
-                Type: {exercise.type}
-                <br />
-                Targets: {exercise.target}</CardBody>
-                <Button onClick={()=>{
-                  handleModalOpen();
-                  setExerciseId(exercise._id);
-                  // console.log(`exercise id is: ${exerciseId}`)
-                  }}>Click to add</Button>
-            </Card>
-            ))}
-        </SimpleGrid>
+          {showResults && ( <> 
+              {exerciseData.map((exercise, index) => (
+              <Card key={exercise._id}>
+                <CardHeader fontWeight="bold" fontSize="large">{exercise.name}</CardHeader>
+                <CardBody>{exercise.description}</CardBody>
+                <CardBody>
+                  Type: {exercise.type}
+                  <br />
+                  Targets: {exercise.target}</CardBody>
+                  <Button onClick={()=>{
+                    handleModalOpen();
+                    setExerciseId(exercise._id);
+                    // console.log(`exercise id is: ${exerciseId}`)
+                    }}>Click to add</Button>
+              </Card>
+              ))}
+              </>
+          )}
+          </SimpleGrid>
       </Box>
       {/* using AddToWorkoutModal component. exerciseId is passed as props to the component
       so we know which exercise to save to the workout the user chooses */}
@@ -173,14 +237,3 @@ export default Browse;
 //     </InputRightAddon>
 //   </InputGroup>
 // </Box>
-
-// {/* collapse bar in testing... not there yet. */}
-// <>
-//   <Button onClick={handleToggle}>
-//     Toggle
-//   </Button>
-//   <Collapse mt={4} isOpen={show}>
-//     {/* Add content to display inside the Collapse */}
-//     <p>Some collapsible content...</p>
-//   </Collapse>
-// </>
