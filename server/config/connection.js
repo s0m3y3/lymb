0e883aae80b09ebf831f438bafbe3641b8d25836
Yee Y <s0m3y3@gmail.com>
2023-11-27 20:13:34 -0600

    .env fixes

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/lymb');
-
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/');
+console.log(process.env.MONGODB_URI)
 module.exports = mongoose.connection;
